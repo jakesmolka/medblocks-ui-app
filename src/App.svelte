47b6f8a6f8b89a5d46676be02e79ac2b32e0eea9
Jake Smolka <30796382+jakesmolka@users.noreply.github.com>
2021-05-02 16:24:51 +0200

    Adds raw json compo retrieval

--- src/App.svelte
+++ src/App.svelte
@@ -8,56 +8,52 @@
 	let ehrIdInput;
 	let compoInput;
 
+	let composition;
+	let data;
+
+	let outputField;
+
+	const ctx = {
+		composer_name: "Dr. Test",
+		territory: "DE",
+	};
+	let defaultEHR = "92fa4396-057d-4d1f-b480-9a83efd0239e";
+
 	let writtenCompoUid = "";
 
 	const axiosAPI = axios.create({
 		baseURL: "http://localhost:8080/ehrbase/rest/ecis/v1",
 	});
 
+	async function submit(e) {
+		// reset bars, if set beforehand
+		alertbar.removeAttribute("open");
+		alertbarError.removeAttribute("open");
+		//loading = true;
+		const r = await form
+			.post(e.detail)
+			.catch((error) => {
+				console.error("Error:", error);
+				alertbarError.setAttribute("open", "");
+			});
+		committed = r.data.compositionUid;
+		writtenCompoUid = r.data.compositionUid;
+				alertbar.setAttribute("open", "");
+		//loading = false;
+	}
+
 	onMount(async () => {
-		ehrIdInput.value = "92fa4396-057d-4d1f-b480-9a83efd0239e";
-
-		form.cdr = axiosAPI;
-		form.template = "AddictionAlcoholTemplate";
-
-		form.addEventListener("submit", (event) => {
-			// reset bars, if set beforehand
-			alertbar.removeAttribute("open");
-			alertbarError.removeAttribute("open");
-
-			form.ehr = ehrIdInput.value;
-
-			form.post(event.detail)
-				.then((r) => {
-					//console.log(JSON.stringify(r));
-					writtenCompoUid = r.data.compositionUid;
-					alertbar.setAttribute("open", "");
-				})
-				.catch((error) => {
-					console.error("Error:", error);
-					alertbarError.setAttribute("open", "");
-				});
-		});
-
-		/* form.addEventListener("load", (event) => {
-			var uuid = compoInput;
-			const composition = form.get(uuid);
-			console.log(composition);
-		}); */
-
-		async function retrieve() {
-			var uuid = compoInput;
-			const composition = await form.get(uuid);
-			console.log(composition);
-			console.log("yeah");
-		}
+		ehrIdInput.value = defaultEHR;
 	});
 
 	async function retrieve() {
-		var uuid = compoInput;
-		//const composition = await form.get(uuid);
-		//console.log(composition);
-		console.log("yeah");
+		const uuid = compoInput.value;
+		const composition = await form.get(uuid);
+		console.log(composition);
+		outputField.value = JSON.stringify(composition);
+		setTimeout(() => {
+			data = composition;
+		});
 	}
 </script>
 
@@ -67,7 +63,9 @@
 	<p>This is an openEHR form app built with Medblocks UI.</p>
 
 	<p>Set EHR ID:</p>
-	<sl-input bind:this={ehrIdInput} placeholder="EHR ID" clearable />
+	<sl-input bind:this={ehrIdInput} placeholder="EHR ID" clearable
+		>{defaultEHR}</sl-input
+	>
 
 	<div>
 		<h2>Template: Addiction Alcohol</h2>
@@ -77,7 +75,16 @@
 			<sl-tab slot="nav" panel="read">Read</sl-tab>
 
 			<sl-tab-panel name="write">
-				<mb-form bind:this={form}>
+				<mb-form
+					bind:this={form}
+					on:mb-input={(e) => (data = e.target.data)}
+					{data}
+					on:submit={submit}
+					template="AddictionAlcoholTemplate"
+					cdr={axiosAPI}
+					ehr="92fa4396-057d-4d1f-b480-9a83efd0239e"
+					{ctx}
+				>
 					<mb-context path="result_report/category" />
 					<!-- <mb-input path="result_report/context/report_id" label="Report ID" />
 				<mb-input path="result_report/context/status" label="Status" /> -->
@@ -189,6 +196,7 @@
 					<mb-submit>Submit</mb-submit>
 				</mb-form>
 			</sl-tab-panel>
+
 			<sl-tab-panel name="read">
 				<p>Set Composition ID:</p>
 				<sl-input
@@ -196,8 +204,10 @@
 					placeholder="Composition ID"
 					clearable
 				/>
-				<sl-button on:click={retrieve()}>Retrieve</sl-button>
+				<sl-button on:click={retrieve}>Retrieve</sl-button>
+				<sl-textarea bind:this={outputField} readonly=true></sl-textarea>
 			</sl-tab-panel>
+
 			<sl-alert bind:this={alertbar} type="success">
 				<sl-icon slot="icon" name="check2-circle" />
 				<strong>Your changes have been saved</strong><br />
